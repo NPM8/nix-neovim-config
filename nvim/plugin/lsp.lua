0b15c6c4d75e34f506c79535513270735ae67186
Dawid 'NPM8' Malysa <nnnp.dawid@gmail.com>
2025-06-11 16:34:11 +0300

    chore: update lsp to proper config

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,4 +1,4 @@
-require("neodev").setup({ })
+require("neodev").setup({})
 
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
@@ -6,47 +6,46 @@ if not status_ok then
 end
 -- imp
 
-local servers = { "gopls", "jsonls", "lua_ls", "pylsp", "tsserver", "yamlls", "astro", "svelte", "nil_ls", "tailwindcss", "intelephense", "volar" }
+local servers = { "gopls", "jsonls", "lua_ls", "pylsp", "tsserver", "yamlls", "astro", "svelte", "nil_ls", "tailwindcss",
+  "intelephense" }
 
 require("mason").setup()
 local masonLsp = require("mason-lspconfig")
 masonLsp.setup({
   ensure_installed = servers,
+  automatic_enable = true,
 })
 
-masonLsp.setup_handlers({
-  function (server_name)
-    local opts = {
-      on_attach = require("user.lsp.handlers").on_attach,
-      capabilities = require("user.lsp.handlers").capabilities,
-    }
-    local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server_name)
-    if has_custom_opts then
-      opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
-    end
-    require('lspconfig')[server_name].setup(opts)
+for _, server_name in ipairs(masonLsp.get_installed_servers()) do
+  local opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
+  }
+  local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server_name)
+  if has_custom_opts then
+    opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
   end
-})
+  require('lspconfig')[server_name].setup(opts)
+end
 
 require("user.lsp.handlers").setup()
 require("lspsaga").setup()
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
- -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
+  debug = false,
+  sources = {
+    formatting.prettier.with({ extra_args = {} }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
     -- diagnostics.flake8
-	},
+  },
 })
-
